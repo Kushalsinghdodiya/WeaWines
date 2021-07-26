f1c4ac551bde0c0fe045581b9f525aa729b0ec60
Danish <dmirza@zolute.consulting>
2021-07-26 19:23:09 +0530

    woocomerce api

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   ToastAndroid,
   StyleSheet,
@@ -6,32 +6,149 @@ import {
   Text,
   TouchableOpacity,
   View,
-  AsyncStorage
+  AsyncStorage,
 } from 'react-native';
 import axios from 'axios';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Icons from 'react-native-vector-icons/Fontisto';
-import { Input, Button } from 'react-native-elements';
+import {Input, Button} from 'react-native-elements';
 import validator from 'validator';
 import Vars from '../utils/Vars';
-export default function SignIn({ route, navigation }) {
+
+import oauthSignature from 'oauth-signature'
+import WooCommerceAPI from 'react-native-woocommerce-api';
+import uuid from "uuid";
+
+export default function SignIn({route, navigation}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const Notify = msg => { ToastAndroid.showWithGravity(msg, ToastAndroid.SHORT, ToastAndroid.BOTTOM); };
-
-
-
- const UserSessionData = async (dt) => {
-    try {
-      console.log("this is setting session details",dt)
-      await AsyncStorage.setItem('user_details',JSON.stringify(dt));
-
-    } catch (error) {
-      console.log("login catch",Error);
-    }
+  
+  const Notify = msg => {
+    ToastAndroid.showWithGravity(msg, ToastAndroid.SHORT, ToastAndroid.BOTTOM);
   };
+  var config = {
+    method: 'get',
+    url: 'http://18.217.240.195/wp-json/wc/v3/products?consumer_key=ck_234a1d928528af0d9db1cdbd3593ec2fe8bd4826&consumer_secret=cs_446bb534522c3354236068c05a1e3c1103acdec0&oauth_consumer_key=ck_234a1d928528af0d9db1cdbd3593ec2fe8bd4826&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1627112840&oauth_nonce=w8k5XLQh57X&oauth_version=1.0&oauth_signature=MNx0ak7GBpp8zSehed9cGnqOwSk%3D',
+    headers: { }
+  };
+  useEffect(() => {
+    const api = new WooCommerceAPI({
+      url: "http://18.217.240.195",
+      consumerKey: "ck_234a1d928528af0d9db1cdbd3593ec2fe8bd4826",
+      consumerSecret: "cs_446bb534522c3354236068c05a1e3c1103acdec0",
+      version: "wc/v3"
+    });
+    
+    // List products
+    api.get("products", {
+      per_page: 20, // 20 products per page
+    })
+      .then((response) => {
+        // Successful request
+        console.log("Response Data:", response);
+      })
+      .catch((error) => {
+        // Invalid request, for 4xx and 5xx statuses
+        console.log("Response Error:", error.response.data);
+      })
+      .finally(() => {
+        // Always executed.
+      });
+    // var CryptoJS = require("crypto-js");
+    // const oauth_timestamp = Math.floor(Date.now() / 1000);
+    // const oauth_nonce = uuid.v1(); 
+    // const parameters = {
+    //     oauth_consumer_key:"ck_234a1d928528af0d9db1cdbd3593ec2fe8bd4826",
+    //     oauth_signature_method:"HMAC-SHA1",
+    //     oauth_timestamp: oauth_timestamp,
+    //     oauth_nonce: oauth_nonce,
+    //     oauth_version:"1.0"
+    // }
+    
+    // function generateEncodedParameters (parameters) {
+    //     let ordered = {};
+    //     Object.keys(parameters).sort().forEach(function(key) {
+    //         ordered[key] = parameters[key];
+    //     });
+        
+    //     let encodedParameters = '';
+    //     for (k in ordered) {
+    //       const encodedValue = escape(ordered[k]);
+    //       const encodedKey = encodeURIComponent(k);
+    //       if(encodedParameters === ''){
+    //          encodedParameters += encodeURIComponent(`${encodedKey}=${encodedValue}`)
+    //       }
+    //       else{
+    //        encodedParameters += encodeURIComponent(`&${encodedKey}=${encodedValue}`);
+    //       }
+    //     }
+    //     console.log(encodedParameters);
+            
+    // }
+    
+    // let encodedParameters = generateEncodedParameters(parameters)
+    
+    
+    // const method = 'GET';
+    // const base_url = 'http://18.217.240.195/wp-json/wc/v3/products?consumer_key=ck_234a1d928528af0d9db1cdbd3593ec2fe8bd4826&consumer_secret=cs_446bb534522c3354236068c05a1e3c1103acdec0';
+    // const encodedUrl = encodeURIComponent(base_url);
+    // encodedParameters = encodeURIComponent(encodedParameters); // encodedParameters which we generated in last step.
+    // const signature_base_string = `${method}&${encodedUrl}&${encodedParameters}`
+    
+    // console.log(signature_base_string)
+    
+    // const secret_key = `cs_446bb534522c3354236068c05a1e3c1103acdec0`;
+    // const signing_key = `${secret_key}&`; //as token is missing in our case.
+    
+    // const oauth_signature =  CryptoJS.HmacSHA1("Message", signing_key)
+    // console.log(oauth_signature);
+    
+    // const encoded_oauth_signature = encodeURIComponent(oauth_signature);
+    // console.log(encoded_oauth_signature);
+    
+    // const authHeader = `OAuth oauth_consumer_key=${secret_key},
+    //     oauth_signature_method='HMAC-SHA1',
+    //     oauth_timestamp=${parameters.oauth_timestamp},
+    //     oauth_nonce=${parameters.oauth_nonce},
+    //     oauth_version=${parameters.oauth_version},
+    //     oauth_signature=${encoded_oauth_signature}`
+    
+    // console.log("outhheader",authHeader)
+    // // var axios = require('axios');
 
+    // var config = {
+    //   method: 'get',
+    //   url: 'http://18.217.240.195/wp-json/wc/v3/products?consumer_key=ck_234a1d928528af0d9db1cdbd3593ec2fe8bd4826&consumer_secret=cs_446bb534522c3354236068c05a1e3c1103acdec0',
+    //   headers: { 
+    //   Authorization:  authHeader
+    //   }
+    // };
+    
+    // axios(config)
+    // .then(function (response) {
+    //   console.log(JSON.stringify(response.data));
+    // })
+    // .catch(function (error) {
+    //   console.log(error);
+    // });
+    
+    // const WooCommerceAPIs = new WooCommerceAPI({
+    //   url: 'http://18.217.240.195/wp-json/wc/v3/', // Your store URL
+    //   ssl: true,
+    //   consumerKey: 'ck_234a1d928528af0d9db1cdbd3593ec2fe8bd4826', // Your consumer secret
+    //   consumerSecret: 'cs_446bb534522c3354236068c05a1e3c1103acdec0', // Your consumer secret
+      
+    //   queryStringAuth: true
+    // });
+    // WooCommerceAPIs.get('products')
+    // .then(data => {
+    //   console.log(data);
+    // })
+    // .catch(error => {
+    //   console.log(error);
+    // });
 
+  });
 
   const handleSubmit = () => {
     const isValid = validator.isEmail(email);
@@ -52,16 +169,14 @@ export default function SignIn({ route, navigation }) {
           method: 'post',
           url: `${Vars.host}login`,
           data: body,
-          headers: { 'Content-Type': 'application/json' },
+          headers: {'Content-Type': 'application/json'},
         })
           .then(function (response) {
             if (response.data.status == 1) {
-
-              console.log("this is sing in ",response.data)
+              console.log('this is sing in ', response.data);
               Notify(response.data.message);
               UserSessionData(response.data.userdetails);
-               navigation.navigate('bottomNavigator');
-              
+              navigation.navigate('bottomNavigator');
             } else {
               Notify(response.data.message);
               return false;
@@ -92,12 +207,12 @@ export default function SignIn({ route, navigation }) {
       <View style={styles.sub_container}>
         <Input
           placeholder="Email"
-          style={{ fontSize: 15, marginBottom: -20 }}
+          style={{fontSize: 15, marginBottom: -20}}
           rightIcon={
             <Icons
               name="email"
               size={20}
-              style={{ marginBottom: -20 }}
+              style={{marginBottom: -20}}
               color="black"
             />
           }
@@ -107,12 +222,12 @@ export default function SignIn({ route, navigation }) {
         <Input
           placeholder="Password"
           secureTextEntry={true}
-          style={{ fontSize: 15, marginBottom: -20 }}
+          style={{fontSize: 15, marginBottom: -20}}
           rightIcon={
             <Icon
               name="eye"
               size={20}
-              style={{ marginBottom: -20 }}
+              style={{marginBottom: -20}}
               color="black"
             />
           }
@@ -133,28 +248,28 @@ export default function SignIn({ route, navigation }) {
         />
 
         <View style={styles.option}>
-          <Text style={{ fontSize: 15 }}>or</Text>
+          <Text style={{fontSize: 15}}>or</Text>
         </View>
         <TouchableOpacity style={styles.social_btn1}>
-          <View style={{ padding: 15, paddingLeft: 20, width: 150 }}>
+          <View style={{padding: 15, paddingLeft: 20, width: 150}}>
             <Image
               width={30}
               height={30}
               source={require('../navigation/assets/google.png')}
             />
           </View>
-          <Text style={{ paddingTop: 15, fontSize: 14 }}>Login With Google</Text>
+          <Text style={{paddingTop: 15, fontSize: 14}}>Login With Google</Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.social_btn1}>
-          <View style={{ padding: 15, paddingLeft: 20, width: 147 }}>
+          <View style={{padding: 15, paddingLeft: 20, width: 147}}>
             <Image
               width={30}
               height={30}
               source={require('../navigation/assets/facebook.png')}
             />
           </View>
-          <Text style={{ paddingTop: 15, fontSize: 14 }}>
+          <Text style={{paddingTop: 15, fontSize: 14}}>
             Login With Facebook
           </Text>
         </TouchableOpacity>
@@ -164,7 +279,7 @@ export default function SignIn({ route, navigation }) {
             <Image
               width={35}
               height={35}
-              style={{ marginTop: 3 }}
+              style={{marginTop: 3}}
               source={require('../navigation/assets/user.png')}
             />
           </View>
@@ -174,7 +289,7 @@ export default function SignIn({ route, navigation }) {
         </View>
 
         <View style={styles.option3}>
-          <Text style={{ color: '#505050' }}>
+          <Text style={{color: '#505050'}}>
             Don't have an account ?
             <Text
               onPress={() => navigation.navigate('SignUp')}
